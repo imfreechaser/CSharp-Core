d85383a6abd2575d4f5b066af3bb00625b8847b3
erina <520175084@qq.com>
2021-06-29 23:29:24 +0800

    static class done

---
+++ Lesson7_封装_静态成员/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson7_封装_静态成员
+{
+    class Person
+    {
+        static Person p = new Person();
+        public int testInt = 10;
+
+        public static Person P
+        {
+            get
+            {
+                return p;
+            }
+        }
+
+        private Person()
+        {
+            
+        }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.P.testInt = 2;
+            Console.WriteLine();
+                
+            
+        }
+    }
+}
