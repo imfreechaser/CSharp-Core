a64b602be0f7a5fe89bcffc491ec93b74941e440
imfreechaser <520175084@qq.com>
2021-07-06 17:18:02 +0800

    task12done

---
+++ Lesson10_封装_运算符重载/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Lesson10_封装_运算符重载
+{
+    struct Position
+    {
+        public float x;
+        public float y;
+        public Position(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool operator ==(Position p1, Position p2)
+        {
+            if (p1.x == p2.x && p1.y == p2.y)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Position p1, Position p2)
+        {
+            if (p1.x == p2.x && p1.y == p2.y)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    class Vector3
+    {
+        public float x, y, z;
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            Vector3 v = new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+            return v;
+        }
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            Vector3 v = new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+            return v;
+        }
+        public static Vector3 operator *(Vector3 v1, float num)
+        {
+            Vector3 v = new Vector3(v1.x * num, v1.y * num, v1.z * num);
+            return v;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p1 = new Position(1, 2);
+            Position p2 = new Position(1, 2);
+            Console.WriteLine(p1 == p2);
+            Console.WriteLine("*********************************************");
+            Vector3 v1 = new Vector3(1, 2, 3);
+            Vector3 v2 = new Vector3(4, 5, 6);
+            Vector3 v = v1 * 5.5f;
+            Console.WriteLine($"{v.x}\t{v.y}\t{v.z}");
+
+
+        }
+    }
+}
--- Lesson11_ssssssssssssssssssdddddddd/Program.cs
+++ Lesson11_ssssssssssssssssssdddddddd/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
 
-namespace Lesson11_ssssssssssssssssssdddddddd
+namespace Lesson12_继承_继承的基本规则
 {
+    class Teacher
+    {
+        public string name;
+        public int number;
+        public void SpeakName()
+        {
+            Console.WriteLine(name);
+        }
+    }
+    class TeachingTeacher : Teacher
+    {
+        public string subject;
+        public void SpeakSubject()
+        {
+            Console.WriteLine(subject + "老师");
+        }
+    }
+    class ChineseTeacher : TeachingTeacher
+    {
+        public void Skill()
+        {
+            Console.WriteLine("念诗");
+        }
+    }
+
+    class Human
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public ushort age;
+        /// <summary>
+        /// 说话行为
+        /// </summary>
+        public void Speak()
+        {
+            Console.WriteLine("每个人都可以说话。");
+        }
+    }
+
+    class Warrior : Human
+    {
+        public void Attack()
+        {
+            Console.WriteLine("战士攻击");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TeachingTeacher tt = new TeachingTeacher();
+            //tt.name = "章老师";
+            //tt.number = 1;
+            //tt.SpeakName();
+            //tt.subject = "历史";
+            //tt.SpeakSubject();
+            //ChineseTeacher ct = new ChineseTeacher();
+            //ct.Skill();
+            //ct.SpeakName();
+
+            Human h1 = new Human();
+            h1.age = 100;
+            h1.name = "张三";
+            h1.Speak();
+            Console.WriteLine("{0} {1}",h1.age,h1.name);
+
+            Warrior w = new Warrior();
+            w.name = "亚瑟";
+            w.age = 33;
+            Console.WriteLine("{0} {1}", w.age, w.name);
+            w.Speak();
+            w.Attack();
+
         }
     }
 }
