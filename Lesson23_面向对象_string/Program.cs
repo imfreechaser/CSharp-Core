8153425082bc7abdc2bc24e0cc6bc36557dd8416
imfreechaser <520175084@qq.com>
2021-07-30 13:54:24 +0800

    task23done

--- Lesson23_面向对象_string/Program.cs
+++ Lesson23_面向对象_string/Program.cs
@@ -6,8 +6,28 @@ namespace Lesson23_面向对象_string
     {
         static void Main(string[] args)
         {
-            string myStr = "12345";
-            Console.WriteLine(myStr.Length);
+            #region Practice1
+            string myStr = "1|2|3|4|5|6|7";
+            string[] str = myStr.Split('|');
+            string myStr1 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = (int.Parse(str[i]) + 1).ToString();
+                Console.WriteLine(str[i]);
+                myStr1 += str[i];
+                if (i != str.Length - 1)
+                    myStr1 += "|";
+            }
+            Console.WriteLine(myStr1);
+            #endregion
+            string str1 = "123";
+            string str2 = str1;
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            str2 = "321";
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+
         }
     }
 }
