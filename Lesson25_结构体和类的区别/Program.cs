cbc340ce451b52b574707134bb7b7b4ffc7c60c3
erina <520175084@qq.com>
2021-08-02 23:58:14 +0800

    lesson26done

---
+++ Lesson25_结构体和类的区别/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson25_结构体和类的区别
+{
+    interface I_Draw
+    {
+        public void DrawMap();
+    }
+     struct map:I_Draw
+    {
+        public static ConsoleColor mapColor;
+        public int length;
+        public map(int length)
+        {
+            this.length = length;
+        }
+        public void DrawMap()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            map m1 = new map();
+            Console.WriteLine(m1.length);
+            m1.length = 10;
+            map.mapColor = ConsoleColor.Red;
+
+            map m2 = new map(12);
+            Console.WriteLine(map.mapColor.ToString());
+        }
+    }
+}
---
+++ Lesson26_抽象类和接口的区别/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson26_抽象类和接口的区别
+{
+    abstract class Animal
+    {
+        public string name;
+        public abstract string catagory
+        {
+            get;
+            set;
+        }
+
+        public abstract void Eat();
+        public void Walk()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
